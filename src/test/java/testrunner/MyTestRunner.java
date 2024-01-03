84c45edc9c5139d89dccfc001cae4dccc355acea
vntptl1994 <ptlvanita1994@gmail.com>
2024-01-03 18:56:55 +0000

    change in runner file

--- src/test/java/testrunner/MyTestRunner.java
+++ src/test/java/testrunner/MyTestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features ={"src/test/resources/AppFeatures/LoginPage.feature"},
+        features ={"src/test/resources/AppFeatures"},
         glue = {"StepDefinitions","AppHooks"},
         plugin = {"pretty",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
